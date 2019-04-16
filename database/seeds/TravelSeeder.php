7b4f7f9d989bd3d0a6f4d3c7611a522971dcf42c
ngo quang nam <ngonam.ptit@gmail.com>
2019-04-16 20:08:34 +0700

    fix travel id

--- database/seeds/TravelSeeder.php
+++ database/seeds/TravelSeeder.php
@@ -12,7 +12,34 @@ class TravelSeeder extends Seeder
      */
     public function run()
     {
-        $category = Category::where('name','street')->get();
+       $category = Category::where('name','street')->get();
+          \App\Models\Travel::create([
+            'category_id' => App\Models\Category::all()->random()->id,
+            'name' => 'Phố bia Tạ Hiện',
+            'place' => 'Tạ Hiện',
+            'feature' => 'Cứ vào thời điểm tối đến, khu phố bia Tạ Hiện này tấp nập hẳn lên, đa số họ là những bạn sinh viên, những người lao động cũng như những khách du lịch từ phương xa tìm đến.',
+            'lat' => '21.035035',
+            'lng' => '105.852183',
+         ]);
+
+         \App\Models\Travel::create([
+                'category_id' => App\Models\Category::all()->random()->id,
+                'name' => 'Royal City',
+                'place' => 'Royal City',
+                'feature' => 'Nếu muốn tìm đến những trung tâm thương mại nổi tiếng nhất ở Hà Nội vào buổi tối để vui chơi, trải nghiệm thì chắc chắn Royal City là địa điểm lý tưởng cho các bạn. Không gian Royal City khá rộng lớn, cách bày trí tạo sự sang trọng và hiện đại vô cùng. Tại đây cũng có nhiều trò chơi thú vị cho các bạn tìm đến, chẳng hạn như trò trượt băng, những trò chơi ở công viên nước hay những bộ phim hay tại rạp chiếu phim đạt tiêu chuẩn quốc tế.',
+                'lat' => '21.002873',
+                'lng' => '105.815268',
+             ]);
+
+              \App\Models\Travel::create([
+                'category_id' => App\Models\Category::all()->random()->id,
+                'name' => 'Chùa Trấn Quốc ở Hà Nội',
+                'place' => 'Hồ Tây, Tây Hồ, Hà Nội, Việt Nam',
+                'feature' => 'Ngôi chùa này đẹp với lối kiến trúc cổ kính, khá linh thiêng cho du khách tìm đến tham quan, trải nghiệm.',
+                'lat' => '21.049698',
+                'lng' => '105.837039',
+             ]);
+
          \App\Models\Travel::create([
                 'category_id' => App\Models\Category::all()->random()->id,
                 'name' => 'phố ẩm thực Hàng Buồm',
@@ -25,20 +52,13 @@ class TravelSeeder extends Seeder
 
               \App\Models\Travel::create([
                 'category_id' => App\Models\Category::all()->random()->id,
-                'name' => 'Phố bia Tạ Hiện',
-                'place' => 'Tạ Hiện',
-                'feature' => 'Cứ vào thời điểm tối đến, khu phố bia Tạ Hiện này tấp nập hẳn lên, đa số họ là những bạn sinh viên, những người lao động cũng như những khách du lịch từ phương xa tìm đến.',
-                'lat' => '21.035035',
-                'lng' => '105.852183',
+                'name' => 'Quảng Trường Ba Đình cùng Lăng Bác',
+                'place' => 'Điện Biên, Điện Bàn, Ba Đình, Hà Nội, Việt Nam',
+                'feature' => 'Quảng Trường Ba Đình rộng lớn, nổi tiếng, được biết đây từng là nơi Bác Hồ đọc tuyên ngôn độc lập, khai sinh ra nước Việt Nam nên rất có giá trị về mặt lịch sử, văn hóa dân tộc nên rất được khách du lịch gần xa yêu thích, tìm đến.',
+                'lat' => '21.036996',
+                'lng' => '105.834635',
              ]);
-              \App\Models\Travel::create([
-                'category_id' => App\Models\Category::all()->random()->id,
-                'name' => 'Royal City',
-                'place' => 'Royal City',
-                'feature' => 'Nếu muốn tìm đến những trung tâm thương mại nổi tiếng nhất ở Hà Nội vào buổi tối để vui chơi, trải nghiệm thì chắc chắn Royal City là địa điểm lý tưởng cho các bạn. Không gian Royal City khá rộng lớn, cách bày trí tạo sự sang trọng và hiện đại vô cùng. Tại đây cũng có nhiều trò chơi thú vị cho các bạn tìm đến, chẳng hạn như trò trượt băng, những trò chơi ở công viên nước hay những bộ phim hay tại rạp chiếu phim đạt tiêu chuẩn quốc tế.',
-                'lat' => '21.002873',
-                'lng' => '105.815268',
-             ]);
+             
               \App\Models\Travel::create([
                 'category_id' => App\Models\Category::all()->random()->id,
                 'name' => ' Hồ Hoàn Kiếm',
@@ -47,22 +67,8 @@ class TravelSeeder extends Seeder
                 'lat' => '21.029005',
                 'lng' => '105.852129',
              ]); 
-              \App\Models\Travel::create([
-                'category_id' => App\Models\Category::all()->random()->id,
-                'name' => 'Quảng Trường Ba Đình cùng Lăng Bác',
-                'place' => 'Điện Biên, Điện Bàn, Ba Đình, Hà Nội, Việt Nam',
-                'feature' => 'Quảng Trường Ba Đình rộng lớn, nổi tiếng, được biết đây từng là nơi Bác Hồ đọc tuyên ngôn độc lập, khai sinh ra nước Việt Nam nên rất có giá trị về mặt lịch sử, văn hóa dân tộc nên rất được khách du lịch gần xa yêu thích, tìm đến.',
-                'lat' => '21.036996',
-                'lng' => '105.834635',
-             ]);
-              \App\Models\Travel::create([
-                'category_id' => App\Models\Category::all()->random()->id,
-                'name' => 'Chùa Trấn Quốc ở Hà Nội',
-                'place' => 'Hồ Tây, Tây Hồ, Hà Nội, Việt Nam',
-                'feature' => 'Ngôi chùa này đẹp với lối kiến trúc cổ kính, khá linh thiêng cho du khách tìm đến tham quan, trải nghiệm.',
-                'lat' => '21.049698',
-                'lng' => '105.837039',
-             ]);
+
+
 
     }
 }
